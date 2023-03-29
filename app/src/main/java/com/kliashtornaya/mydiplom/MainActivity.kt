d30fe3cfa9eded289e8e5ebad7354d5b483e4604
Yulya-1@tut.by <Yulya-1@tut.by>
2023-03-29 13:10:58 +0300

    adding text to buttons and images

--- app/src/main/java/com/kliashtornaya/mydiplom/MainActivity.kt
+++ app/src/main/java/com/kliashtornaya/mydiplom/MainActivity.kt
@@ -2,10 +2,35 @@ package com.kliashtornaya.mydiplom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initMenuButtons()
+    }
+    private fun initMenuButtons() {
+        var buttonMenu=findViewById<Button>(R.id.button_menu)
+        buttonMenu.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        var buttonFavorites=findViewById<Button>(R.id.button_favorites)
+        buttonFavorites.setOnClickListener {
+            Toast.makeText(this, "ИЗБРАННОЕ", Toast.LENGTH_SHORT).show()
+        }
+        var buttonViewLater=findViewById<Button>(R.id.button_view_later)
+        buttonViewLater.setOnClickListener {
+            Toast.makeText(this, "ПОСМОТРЕТЬ ПОЗЖЕ", Toast.LENGTH_SHORT).show()
+        }
+        var buttonSelections=findViewById<Button>(R.id.button_selections)
+        buttonSelections.setOnClickListener {
+            Toast.makeText(this, "ПОДБОРКИ", Toast.LENGTH_SHORT).show()
+        }
+        var buttonSetting=findViewById<Button>(R.id.button_settings)
+        buttonSetting.setOnClickListener {
+            Toast.makeText(this, "НАСТРОЙКИ", Toast.LENGTH_SHORT).show()
+        }
     }
 }
